16f312971957327a569fbbcd3b31fd4e9fc5c274
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2023-09-18 22:54:29 +0530

    Time: 2 ms (89.35%) | Memory: 48.6 MB (93.52%) - LeetSync

--- 1491-number-of-times-binary-string-is-prefix-aligned/number-of-times-binary-string-is-prefix-aligned.java
+++ 1491-number-of-times-binary-string-is-prefix-aligned/number-of-times-binary-string-is-prefix-aligned.java
@@ -3,12 +3,12 @@ class Solution {
      
         
         int cnt=0,max=0;
-        HashSet<Integer> h= new HashSet<>();
+        // HashSet<Integer> h= new HashSet<>();
         
         for(int i=0;i<flips.length;i++){ 
-            h.add(flips[i]);
+            // h.add(flips[i]);
             max=Math.max(flips[i],max);
-            if(h.size()==max){
+            if(i+1==max){
                 cnt++;
             }
             
