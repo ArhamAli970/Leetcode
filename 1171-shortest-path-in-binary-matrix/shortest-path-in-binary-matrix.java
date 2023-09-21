ec0eafc3b1f85139bfe2e87df82275c1e8b588e3
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2023-09-21 15:58:40 +0530

    Time: 43 ms (8.67%) | Memory: 44.5 MB (74.45%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,44 +1,136 @@
-class Solution {
-    static class node{ 
-    int i,j;
-    node(int i,int j){ 
-    this.i=i;
-    this.j=j;
-    }
+class Solution { 
+    class node{ 
+        int i;
+        int j;
+        node(int i,int j){ 
+            this.i=i;
+            this.j=j;
+        }
+
     }
     public int shortestPathBinaryMatrix(int[][] grid) {
-         int n=grid.length;
+        int n=grid.length;
         if(grid[0][0]==1 || grid[n-1][n-1]==1){return -1;}
-        
-        Queue<node> q= new LinkedList<>();
-       
-        
-        int sp=0;
-        q.add(new node(0,0));
-        
+        int [][]mat= new int[n][n];
+        boolean[][] bool= new boolean[n][n];
+
+        mat[0][0]=1;
+        Queue<node> q=new LinkedList<>();
+
+        q.add(new node(0,0)); 
+
+
         while(!q.isEmpty()){ 
-            
-            int size=q.size();
-            
-            for(int i=0;i<size;i++){ 
-             node curr=q.remove();
-             int x=curr.i,y=curr.j;
-             if(x==n-1 && y==n-1){return sp+1;}
-             
-             if(x-1>=0 && grid[x-1][y]==0){ grid[x-1][y]=1; q.add(new node(x-1,y)); }
-             if(y-1>=0 && grid[x][y-1]==0){ grid[x][y-1]=1; q.add(new node(x,y-1)); }
-             if(x+1<n && grid[x+1][y]==0){ grid[x+1][y]=1; q.add(new node(x+1,y)); }
-             if(y+1<n && grid[x][y+1]==0){ grid[x][y+1]=1; q.add(new node(x,y+1)); }  
-             if(x-1>=0 && y-1>=0 && grid[x-1][y-1]==0){ grid[x-1][y-1]=1; q.add(new node(x-1,y-1));}
-             if(x-1>=0 && y+1<n  && grid[x-1][y+1]==0){ grid[x-1][y+1]=1; q.add(new node(x-1,y+1)); }
-             if(x+1<n && y+1<n && grid[x+1][y+1]==0){ grid[x+1][y+1]=1; q.add(new node(x+1,y+1)); }
-             if(y-1>=0 && x+1<n  && grid[x+1][y-1]==0){ grid[x+1][y-1]=1; q.add(new node(x+1,y-1)); }
-                
-            
-            }
-            sp++;
+int size=q.size();
+for(int k=0;k<size;k++){
+node curr=q.remove();
+int i=curr.i;
+int j=curr.j;
+int val=mat[i][j];
+bool[i][j]=true;
+
+if(i-1>=0 && j-1>=0 && grid[i-1][j-1]==0 && !bool[i-1][j-1]){ 
+    if(mat[i-1][j-1]!=0){
+        mat[i-1][j-1]=Math.min(val+1,mat[i-1][j-1]);
         }
+        else{ 
+            mat[i-1][j-1]=val+1;
+            q.add(new node(i-1,j-1));
+        }
+}
+
+if(i-1>=0 && grid[i-1][j]==0 && !bool[i-1][j]){ 
+    if(mat[i-1][j]!=0){
+        mat[i-1][j]=Math.min(val+1,mat[i-1][j]);
+        }
+        else{ 
+            mat[i-1][j]=val+1;
+            q.add(new node(i-1,j));
+        }
+}
+
+
+if(i-1>=0 && j+1<n && grid[i-1][j+1]==0 && !bool[i-1][j+1]){ 
+    if(mat[i-1][j+1]!=0){
+        mat[i-1][j+1]=Math.min(val+1,mat[i-1][j+1]);
+        }
+        else{ 
+            mat[i-1][j+1]=val+1;
+            q.add(new node(i-1,j+1));
+        }
+}
+
+
+
+
+
+if(j+1<n && grid[i][j+1]==0 && !bool[i][j+1]){ 
+    if(mat[i][j+1]!=0){
+        mat[i][j+1]=Math.min(val+1,mat[i][j+1]);
+        }
+        else{ 
+            mat[i][j+1]=val+1;
+            q.add(new node(i,j+1));
+        }
+}
+
+
+if(j-1>=0 && grid[i][j-1]==0 && !bool[i][j-1]){ 
+    if(mat[i][j-1]!=0){
+        mat[i][j-1]=Math.min(val+1,mat[i][j-1]);
+        }
+        else{ 
+            mat[i][j-1]=val+1;
+            q.add(new node(i,j-1));
+        }
+}
+
+
+
+if(i+1<n && j-1>=0 && grid[i+1][j-1]==0 && !bool[i+1][j-1]){ 
+    if(mat[i+1][j-1]!=0){
+        mat[i+1][j-1]=Math.min(val+1,mat[i+1][j-1]);
+        }
+        else{ 
+            mat[i+1][j-1]=val+1;
+            q.add(new node(i+1,j-1));
+        }
+}
+
+
+if(i+1<n  && grid[i+1][j]==0 && !bool[i+1][j]){ 
+    if(mat[i+1][j]!=0){
+        mat[i+1][j]=Math.min(val+1,mat[i+1][j]);
+        }
+        else{ 
+            mat[i+1][j]=val+1;
+            q.add(new node(i+1,j));
+        }
+}
+
+
+if(i+1<n && j+1<n && grid[i+1][j+1]==0 && !bool[i+1][j+1]){ 
+    if(mat[i+1][j+1]!=0){
+        mat[i+1][j+1]=Math.min(val+1,mat[i+1][j+1]);
+        }
+        else{ 
+            mat[i+1][j+1]=val+1;
+            q.add(new node(i+1,j+1));
+        }
+}
+
+
+
+
+
+
+
+}
+ 
+        }
+
         
-        return -1;
+
+return mat[n-1][n-1]==0?-1:mat[n-1][n-1];
     }
 }
