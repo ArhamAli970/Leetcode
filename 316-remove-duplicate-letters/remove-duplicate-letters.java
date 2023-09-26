c343481eda31eca1ef85cc4ff3084221b7ea4a7e
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2023-09-26 14:48:10 +0530

    Time: 2 ms (96.28%) | Memory: 40.8 MB (92.52%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        
+  int n=s.length();       
+  int cntChar[]= new int[26];
+  boolean chk[]= new boolean[26];
+  Deque<Character> dq= new LinkedList<>();
+
+  for(int i=0;i<n;i++){ 
+    ++cntChar[s.charAt(i)-'a'];
+  }
+
+  for(int i=0;i<n;i++){ 
+    int idx=s.charAt(i)-'a';
+    if(!chk[idx]){ 
+      while(!dq.isEmpty() && dq.getLast()>s.charAt(i) && cntChar[dq.getLast()-'a']>0){ 
+          chk[dq.removeLast()-'a']=false;
+      }
+      dq.add(s.charAt(i));
+      chk[idx]=true;
+    }
+     --cntChar[idx];
+  }
+
+StringBuilder sb= new StringBuilder();
+ while(!dq.isEmpty()){ 
+   sb.append(dq.remove());
+ }
+
+ return sb.toString();
+
+        
+    }
+}
