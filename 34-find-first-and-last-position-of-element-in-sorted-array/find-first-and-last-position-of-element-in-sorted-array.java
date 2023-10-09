c27ff146c4e05991dfd0fafd9042dc39f12a4e68
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2023-10-09 21:00:07 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (91.41%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,45 +1,48 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int arr[]= new int[2];
-        if (nums.length==1 && target==nums[0]){ 
-            return arr;
-        }
-
-
-arr[0]=100000000;
-arr[1]=-1;
-int i=0;
-int j=nums.length-1;
-while (i<=j){ 
-    if (nums[j]==target && j>arr[1]){ 
-        arr[1]=j;
-    }
-     else if(nums[j]==target && j<arr[0]){ 
-        arr[0]=j;
-    }
-    if (nums[i]==target && i<arr[0] ){ 
-        arr[0]=i;
-    }
-    else if(nums[i]==target && i>arr[1]){ 
-        arr[1]=i;
-    }
-
-    i++;
-    j--;
-}
-
-if (arr[0]==100000000 && arr[1]==-1){ 
-    arr[0]=-1;
-}
-else if (arr[0]!=100000000 && arr[1]==-1){ 
-    arr[1]=arr[0];
-}
-else if(arr[0]==100000000 && arr[1]!=-1){ 
-    arr[0]=arr[1];
-}
-
-
-return arr;
+    public int[] searchRange(int[] arr, int x) {
+        int ans[]=new int[2];
+        int n=arr.length;
         
+        int foundLast=-1,foundFirst=-1;
+        
+        int st=0,end=n-1;
+        while(st<=end){ 
+         
+         int mid=(st+end)/2;
+         
+         if(arr[mid]<x){ 
+          st=mid+1;   
+         }
+         else if(arr[mid]>x){ 
+          end=mid-1;   
+         }
+         else{ 
+          foundLast=mid;
+          st=mid+1;
+         }
+        } 
+        
+        
+          st=0;end=n-1;
+        while(st<=end){ 
+         
+         int mid=(st+end)/2;
+         
+         if(arr[mid]<x){ 
+          st=mid+1;   
+         }
+         else if(arr[mid]>x){ 
+          end=mid-1;   
+         }
+         else{ 
+          foundFirst=mid;
+          end=mid-1;
+         }
+        }
+        
+        
+        ans[0]=foundFirst;
+        ans[1]=foundLast;
+        return ans;  
     }
 }
