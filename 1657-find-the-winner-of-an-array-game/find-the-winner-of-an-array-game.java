584e3ba5bc63085c4161de7ce20eaaab73e75dab
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2023-11-05 07:54:31 +0530

    Time: 1 ms (52.90%) | Memory: 56.5 MB (55.80%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.java
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.java
@@ -2,25 +2,24 @@ class Solution {
     public int getWinner(int[] arr, int k) {
      
      if(k==1){return Math.max(arr[0],arr[1]);}
-     int max=0,n=arr.length;
-     for(int i=0;i<n;i++){ 
-           max=Math.max(arr[i],max);
-     }
+     int n=arr.length;
 
-     if(k>=arr.length-1){ 
-         return max;
-     }
-        
+      // above are the end cases  
         int lst=arr[0],c=0;
+        int max=arr[0];
      for(int i=0;i<n;){ 
-         
+         // we will loop inner loop and check the cnt till the lst number is Largest 
          while(i+1<n && arr[++i]<lst){ 
+             max=Math.max(arr[i],max);
              c++; if(c==k){return lst;}
          }
+         // we take c=1 here because the number we got now is become largest from the lst one  so it will be mark as count 1 and then we check again
      lst=arr[i];c=1;
+     max=Math.max(lst,max);
      if(i+1==n){break;}
      }
 
+// if nothing was determin with the loop we will return max
 return max;
 
     }
