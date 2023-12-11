64c41d7844af7fdf3345d4fab4f2e88d7d49eb52
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2023-12-11 19:02:47 +0530

    Time: 126 ms (5.98%) | Memory: 54.9 MB (17.95%) - LeetSync

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minSwaps(String s) {
+        int n=s.length(),i=0;
+        Stack<Character> st=new Stack<>();
+
+        while(i<n){ 
+
+            if(s.charAt(i)=='['){st.push(s.charAt(i));}
+
+            else{
+            if(!st.isEmpty() && st.peek()!=']'){ 
+                st.pop();
+            }else{ 
+                st.push(']');
+            }
+            }
+i++;
+        }
+int cnt=st.size()/2;
+
+return cnt%2==0?cnt/2:(cnt/2)+1;
+
+
+    }
+}
