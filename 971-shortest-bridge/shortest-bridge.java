990f886a40d3f31a864bb5a057cd48e28d4c3d1c
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2023-12-27 23:53:35 +0530

    Time: 15 ms (23.81%) | Memory: 45.7 MB (5.36%) - LeetSync

--- 971-shortest-bridge/shortest-bridge.java
+++ 971-shortest-bridge/shortest-bridge.java
@@ -1,110 +1,95 @@
 class Solution {
-
-static class node { 
-    int i;
-    int j;
-    node(int i,int j){ 
-        this.i=i;
-        this.j=j;
+    static class node{ 
+        int i, j;
+        node(int i,int j){ 
+            this.i=i;
+            this.j=j;
+        }
     }
-}    
+    static Queue<node> q;
+    static    boolean chk[][];
+    public static void dfs(int i,int j,int grid[][],int val){ 
+        if(i<0 || j<0 || i==grid.length || j==grid.length  ||  grid[i][j]!=1){
+            return;
+        }
+        grid[i][j]=val;
+        // chk[i][j]=true;
+        q.add(new node(i,j));
+        dfs(i+1,j,grid,val);
+        dfs(i,j+1,grid,val);
+        dfs(i,j-1,grid,val);
+        dfs(i-1,j,grid,val);
 
-
-public static void dfs1(int i,int j,int mat[][],Queue<node> q,boolean vis[][]){ 
-    if(i<0|| j<0|| i==mat.length || j==mat[0].length || mat[i][j]==0 || vis[i][j]){ 
-return;
     }
 
-    vis[i][j]=true;
-q.add(new node(i, j));
-dfs1(i+1,j,mat,q,vis);  
-dfs1(i,j-1,mat,q,vis);  
-dfs1(i-1,j,mat,q,vis);  
-dfs1(i,j+1,mat,q,vis);  
+    public int shortestBridge(int[][] grid) {
+        int n=grid.length;
+        q=new LinkedList<>();
+        // chk=new boolean[n][n];
+        int done=-1;
+        int val=-1;
+        for(int i=0;i<n;i++){ 
+            for(int j=0;j<n;j++){ 
+
+                if(grid[i][j]==1){ 
+
+                    dfs(i,j,grid,val); done=1; break;
+                   
+                }
 
 
-}
+
+            }
+            if(done==1){break;}
+    
+        }
+
+      
+
+int cnt=0;
+        while(!q.isEmpty()){ 
+
+            int size=q.size();
+
+            for(int k=0;k<size;k++){ 
+
+                node curr=q.remove();
+                int i=curr.i,j=curr.j;
+                //   System.out.print(i+""+j+" ");
+
+                if(grid[i][j]==1){return cnt-1;}
+               
+
+                // grid[i][j]=-1;
+
+                if(i-1>=0 && grid[i-1][j]!=-1){
+                    grid[i-1][j]=grid[i-1][j]==1?1:-1;
+                    q.add(new node(i-1,j)); }
+                if(j-1>=0 && grid[i][j-1]!=-1){
+                     grid[i][j-1]=grid[i][j-1]==1?1:-1;
+                    q.add(new node(i,j-1)); 
+                    }
+                if(i+1<n &&  grid[i+1][j]!=-1){
+                     grid[i+1][j]=grid[i+1][j]==1?1:-1;
+                    q.add(new node(i+1,j)); 
+                    }
+                if(j+1<n && grid[i][j+1]!=-1){
+                     grid[i][j+1]=grid[i][j+1]==1?1:-1;
+                    q.add(new node(i,j+1)); 
+                    }
 
 
-    public int shortestBridge(int[][] mat) {
-        Queue<node> q= new LinkedList<>();
+                
 
-
-   int n=mat.length;
-   boolean vis[][]= new boolean[n][n];
-boolean dfs=true;
-   for(int i=0;i<n;i++){ 
-    for (int j=0;j<n;j++){ 
-        if(mat[i][j]==1){ 
-            dfs1(i,j,mat,q,vis);   
-            dfs=false;
-            break;
+            }
+            cnt++;
 
         }
-      
-    }
-    if(!dfs){break;}
-   }
-
-
-
-//    for(int i=0;i<n;i++){ 
-//     for (int j=0;j<n;j++){ 
-       
-//       System.out.print(vis[i][j]);
-//     }
-//  System.out.println();
-//    }
-
-
-
-int step=0;
-boolean stp=false;
-
-   while(!q.isEmpty()){ 
-
-    int size=q.size();
-
-    for (int k=0;k<size;k++){ 
-
-node curr= q.remove();  
-int i=curr.i;
-int j=curr.j;
-
-if(mat[i][j]==1 && !vis[i][j]){ 
-   return step-1;
-   
-}
-
-if(i-1>=0 && !vis[i-1][j] ){ 
-    q.add(new node(i-1, j)); 
-   if(mat[i-1][j]!=1) vis[i-1][j]=true;
-}
-if(j-1>=0 && !vis[i][j-1]){ 
-    q.add(new node(i, j-1));
-    if(mat[i][j-1]!=1)  vis[i][j-1]=true;
-    
-}
-if(i+1<n && !vis[i+1][j] ){ 
-    q.add(new node(i+1, j));
-    if(mat[i+1][j]!=1)  vis[i+1][j]=true;
-}
-if(j+1<n && !vis[i][j+1]){ 
-    q.add(new node(i, j+1));
-    if(mat[i][j+1]!=1)  vis[i][j+1]=true;
-}
-
-
-
-    } 
- 
-   
-    step++;
-
-   }
-
 
 
 return -1;
+
+
+        
     }
 }
