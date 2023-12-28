a747c163890f635f2363eb388d3a476146f7fcee
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2023-12-28 15:37:16 +0530

    Time: 61 ms (16.91%) | Memory: 44.9 MB (8.63%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.java
+++ 1129-longest-string-chain/longest-string-chain.java
@@ -1,42 +1,50 @@
 class Solution {
-    static int dp[];
-    public static int dfs(int i,String[] words){ 
+    public static int dp[];
 
-      if(dp[i]!=-1){return dp[i];}
-      int max=0;
+public static int wordChk(String w1,String w2){ 
 
-      for(int j=0;j<words.length;j++){
-          if(words[i].length()!=words[j].length()-1){continue;}
-          if(i==j){continue;} 
-          
-          int l=0,m=0;
-          while( l<words[i].length() &&  m<words[j].length()){ 
-              if(words[i].charAt(l)==words[j].charAt(m)){ 
-                  l++;
-              }
-              m++;
-          }
+int i=0,j=0,cnt=0,n=w1.length(),m=w2.length();
 
-          if(l==words[i].length()){ 
-            max=Math.max(dfs(j,words)+1,max);
-          }
 
-      }
-      dp[i]=max;
-
-      return dp[i];
-
+while(i<n && j<m){ 
+    if(w1.charAt(i)!=w2.charAt(j)){ 
+        j++;cnt++;
+    }else{ 
+        i++;j++;
     }
+}
+
+if(j==m-1){cnt++;}
+
+return cnt==1?1:0;
+
+}
 
     public int longestStrChain(String[] words) {
-        dp= new int[words.length];
-        Arrays.fill(dp,-1);
-        int max=0;
-        for(int i=0;i<words.length;i++){ 
-            max=Math.max(dfs(i,words),max);
+
+         Arrays.sort(words,(c,b)->{ 
+            return c.length()-b.length();
+        });
+        int n=words.length;
+        
+        dp= new int[n];
+        int maxAns=0;
+
+
+        for(int i=0;i<n;i++){ 
+            int max=0;
+            for(int j=0;j<i;j++){ 
+
+                if(words[j].length()+1 ==words[i].length() && wordChk(words[j],words[i])==1){ 
+                    max=Math.max(dp[j],max);
+                }
+
+            }
+              dp[i]=max+1;
+            maxAns=Math.max(dp[i],maxAns);
+          
         }
 
-        return max+1;
-
+return maxAns;
     }
 }
