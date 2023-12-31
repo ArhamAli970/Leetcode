8656312855a77649f254a1ed9858e35493edac34
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2023-12-31 21:55:18 +0530

    Time: 24 ms (30.06%) | Memory: 42.7 MB (26.19%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,25 +1,26 @@
 class Solution {
-    static int dp[][][];
+    static int dp[][];
 
     public static int call(int l,int m,int st,int end,int[] cuts){ 
         if(l>m){return 0;}
         int min=Integer.MAX_VALUE;
 
+        if(dp[l][m]!=-1){return dp[l][m];}
+
         for(int i=l;i<=m;i++){ 
-            if(dp[l][m][i]==-1){
+
             int take1=call(l,i-1,st,cuts[i],cuts);
             int take2=call(i+1,m,cuts[i],end,cuts);
-            dp[l][m][i]=take1+take2+(end-st);
-            }
+            min=Math.min(take1+take2+(end-st),min);
 
-            min=Math.min(dp[l][m][i],min);
+            // min=Math.min(dp[l][m],min);
 
 
                      
         }
 
-        return min;
-
+        dp[l][m]=min;
+return dp[l][m];
     }
 
 
@@ -27,13 +28,13 @@ class Solution {
         int l= cuts.length;
         Arrays.sort(cuts);
 
-        dp= new int[l][l][l];
+        dp= new int[l][l];
 
-        for(int i=0;i<l;i++){ 
-            for(int j[]:dp[i]){ 
-                Arrays.fill(j,-1);
+        
+            for(int i[]:dp){ 
+                Arrays.fill(i,-1);
             }
-        }
+        
 
         return call(0,l-1,0,n,cuts);
     
