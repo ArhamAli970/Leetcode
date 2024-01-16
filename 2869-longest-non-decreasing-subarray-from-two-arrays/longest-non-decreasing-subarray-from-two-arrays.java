64d4d5f4b4261f2dd3bd9f1fe3e4791440420959
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2024-01-16 11:24:43 +0530

    Time: 17 ms (32.19%) | Memory: 61.9 MB (38.13%) - LeetSync

---
+++ 2869-longest-non-decreasing-subarray-from-two-arrays/longest-non-decreasing-subarray-from-two-arrays.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maxNonDecreasingLength(int[] nums1, int[] nums2) {
+        int n=nums1.length;
+        // System.out.print(nums1.length+" "+nums2.length);
+        int dp[][]= new int[n][2];
+
+        dp[0][0]=1;
+        dp[0][1]=1;
+        int max=1;
+
+        for(int i=1;i<n;i++){ 
+        
+            if(nums1[i]>=nums1[i-1]){
+                dp[i][0]=Math.max(dp[i-1][0]+1,dp[i][0]);
+            }
+            if(nums1[i]>=nums2[i-1]){
+                dp[i][0]=Math.max(dp[i-1][1]+1,dp[i][0]);
+            }
+
+
+            if(nums2[i]>=nums1[i-1]){
+                dp[i][1]=Math.max(dp[i-1][0]+1,dp[i][1]);
+            }
+             if(nums2[i]>=nums2[i-1]){
+                dp[i][1]=Math.max(dp[i-1][1]+1,dp[i][1]);
+            }
+
+            dp[i][0]=Math.max(dp[i][0],1);
+            dp[i][1]=Math.max(dp[i][1],1);
+            
+            max=Math.max(max,Math.max(dp[i][0],dp[i][1]));
+        }
+        
+        return max;
+    }
+}
