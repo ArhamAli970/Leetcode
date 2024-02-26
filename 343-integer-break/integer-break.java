a14094ca4709d9f3f7eaaf247541e887255cae6c
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2024-02-26 15:43:06 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (34.04%) - LeetSync

--- 343-integer-break/integer-break.java
+++ 343-integer-break/integer-break.java
@@ -1,22 +1,24 @@
 class Solution {
+    static int dp[];
+    public static int call(int n,int k){
+        if(n<=0 && k<2){return 0;}
+        if(n==0 || n==1){return 1;}
+
+if(dp[n]!=-1){return dp[n];}
+
+int max=0;
+        for(int i=1;i<=n;i++){
+max=Math.max(i*call(n-i,k+1),max);
+        }
+        dp[n]=max;
+return dp[n];
+    
+    }
     public int integerBreak(int n) {
+    dp=new int[n+1]; 
+    Arrays.fill(dp,-1);   
+    return call(n,0);
 
-if(n==2 || n==3){return n-1;}
-
- int max=0;
-     for(int i=1;i<=n;i++){ 
-           
-           int div=n/i;
-           int rem=n%i;
-
-           int pow1=(int)Math.pow(div,i-rem);
-           int pow2=(int)Math.pow(div+1,rem);
-           max=Math.max(max,pow1*pow2);
-
-
-     }
-
-    return max;
 
     }
 }
