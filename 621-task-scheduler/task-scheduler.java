07867e66d004367d9cc72f205d2e773a57e105df
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2024-03-19 07:33:11 +0530

    Time: 140 ms (5.10%) | Memory: 45.6 MB (56.73%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -4,8 +4,11 @@ class Solution {
         int wait[]= new int[26];
         int cnt[]=new int[26];
 
+        HashSet<Integer> h= new HashSet<>();
+
         for(int i=0;i<tasks.length;i++){
             ++cnt[tasks[i]-'A'];
+            h.add(tasks[i]-'A');
         }
 
         int t=tasks.length,time=0;
@@ -19,16 +22,33 @@ class Solution {
                     maxIdx=i;
                 }
             }
-            // System.out.print(maxIdx+" ");
+
              
             for(int i=0;i<26;i++){
                 if(wait[i]>0){--wait[i];}
             }
-            if(maxIdx==-1){continue;}
+            
 
             wait[maxIdx]=n;
             --cnt[maxIdx];
             --t;
+
+            if(t==0){break;}
+
+            int min=Integer.MAX_VALUE;
+            for(int i=0;i<26;i++){
+               if(h.contains(i) && cnt[i]>0){min=Math.min(wait[i],min);}
+            }
+                        // System.out.println(maxIdx+" "+min);
+
+            for(int i=0;i<26;i++){
+               if(h.contains(i)){wait[i]-=min;}
+            }
+
+            // System.out.print(min+" ");
+
+            time+=min;
+
 
         }
 
