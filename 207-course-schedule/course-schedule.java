6958a37b51951c473db5fcd947eb96c3147e2e4a
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2024-07-06 18:36:16 +0530

    Time: 4 ms (79.16%) | Memory: 44.4 MB (71.01%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,50 +1,37 @@
 class Solution {
-static boolean vis[];
-public static boolean call(int i, ArrayList<Integer> arr[],HashSet<Integer> curr){
-    if(vis[i]){return true;}
-    vis[i]=true;
-    curr.add(i);
+    public boolean canFinish(int n, int[][] pre) {
 
-    for(int j=0;j<arr[i].size();j++){
-        int now=arr[i].get(j);
-        if(curr.contains(now)){return false;}
-        else if(!call(now,arr,curr)){
-            return false;
-        }
-    }
-    curr.remove(i);
-    return true;
-}
-
-    public boolean canFinish(int numCourses, int[][] pre) {
-        int n=numCourses;
-        ArrayList<Integer> arr[]= new ArrayList[n];
+        int deg[]= new int[n];
+        ArrayList<Integer> arr[]=new ArrayList[n];
 
         for(int i=0;i<n;i++){
             arr[i]=new ArrayList<>();
         }
 
-        for(int i=0;i<pre.length;i++){
-            arr[pre[i][1]].add(pre[i][0]);
+        for(int i[]:pre){
+            arr[i[1]].add(i[0]);
+            ++deg[i[0]];
         }
 
-         vis= new boolean[n];
-         HashSet<Integer> curr= new HashSet<>();
-     
-
+        Queue<Integer> q= new LinkedList<>();
 
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                if(!call(i,arr,curr)){
-                    return false;
-                }
-            }
-
+            if(deg[i]==0){q.add(i);}
         }
 
-return true;
-        
-    
+int cnt=0;
+        while(!q.isEmpty()){
+            int curr=q.remove();
+            cnt++;
+            for(int i=0;i<arr[curr].size();i++){
+                --deg[arr[curr].get(i)];
+                if(deg[arr[curr].get(i)]==0){q.add(arr[curr].get(i));}
+            }
+        }
 
+
+return cnt==n;
+
+        
     }
 }
