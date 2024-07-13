9e76c162f2fe10b1f78422bdaed5d7ba49e47f74
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2024-07-13 09:05:32 +0530

    Time: 99 ms (17.50%) | Memory: 93.2 MB (6.25%) - LeetSync

--- 2846-robot-collisions/robot-collisions.java
+++ 2846-robot-collisions/robot-collisions.java
@@ -1,100 +1,69 @@
 class Solution {
-    
     public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
-        
-        int n=healths.length;
-        TreeMap<Integer,ArrayList<Integer>> mp = new TreeMap<>();
-        
-        for(int i=0;i<n;i++){ 
-            
-            mp.put(positions[i],new ArrayList<>());
-            mp.get(positions[i]).add(i);
-            mp.get(positions[i]).add(healths[i]);
-            int dir= directions.charAt(i)=='R'?1:-1;
-            mp.get(positions[i]).add(dir);
-        
+        int n=positions.length;
+        int arr[][]=new int[n][2];
+        HashMap<Integer,Integer> mp=new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            mp.put(positions[i],i);
+            arr[i][0]=positions[i];
+            arr[i][1]=healths[i];
+            healths[i]=-1;
+            arr[i][1]=directions.charAt(i)=='R'?arr[i][1]:0-arr[i][1];
         }
-        
-        
-        ArrayList<ArrayList<Integer>> arr=new ArrayList<>(); 
-        for(Map.Entry<Integer,ArrayList<Integer>> e:mp.entrySet()){ 
-           arr.add(e.getValue());
-        }
-        
-        // System.out.print(mp);
-        
-        Stack<ArrayList<Integer>> st=new Stack<>();
-        
-        for(int i=n-1;i>=0;i--){ 
-            
-  
-            if((st.isEmpty()) || (arr.get(i).get(2)==st.peek().get(2)) || arr.get(i).get(2)==-1){ 
-                st.push(arr.get(i));
-            }
-            
-            else if(arr.get(i).get(2)==1 && st.peek().get(1)>arr.get(i).get(1)){
-                ArrayList<Integer> a= st.pop();
-                int val=a.get(1);
-                a.set(1,val-1);
-                st.push(a);
-            }
 
-             else if(arr.get(i).get(2)==1 && st.peek().get(1)==arr.get(i).get(1)){
-               st.pop();
-            }
-
-            else{ 
-                int hlth=arr.get(i).get(1);
-                while(!st.isEmpty() && st.peek().get(2)!=1 && hlth>st.peek().get(1)){ 
-                    st.pop();
-                    hlth--;
-                }
-
-                if(st.isEmpty() || st.peek().get(2)==1){ 
-                     arr.get(i).set(1,hlth);
-                     st.push(arr.get(i));
-                }
-                else if(hlth==st.peek().get(1)){ 
-                    st.pop();
-                }
-                else if (hlth<st.peek().get(1)){ 
-                ArrayList<Integer> a= st.pop();
-                int val=a.get(1);
-                a.set(1,val-1);
-                st.push(a);
-                }
-            }
-
-
-        
-        
-        }
-        
-        
-        arr=new ArrayList<>();
-        // System.out.print(st.size());
-        while(!st.isEmpty()){ 
-        arr.add(st.pop());
-        }
-        
-        Collections.sort(arr,(e1,e2)->{ 
-          return e1.get(0)-e2.get(0);
+        Arrays.sort(arr,(a,b)->{
+            return a[0]-b[0];
         });
-            
-            
+
+
+        Stack<ArrayList<Integer>> st=new Stack<>();
+
+        for(int i=0;i<n;i++){
+            int val=arr[i][1],chk=1;
+            // while(!st.isEmpty() && st.peek().get(0)<0 && val>0){
+            //     if(val==Math.abs(st.peek().get(0))){st.pop();chk=-1;break;}
+            //     else if(val>Math.abs(st.peek().get(0))){st.pop();--val;}
+            //     else{st.peek().set(0,Math.abs(st.peek().get(0))-1); chk=-1;break;}
+            // }
+
+            while(!st.isEmpty() && st.peek().get(0)>0 && val<0){
+                if(Math.abs(val)==st.peek().get(0)){st.pop();chk=-1;break;}
+                else if(Math.abs(val)>st.peek().get(0)){st.pop();++val;}
+                else{st.peek().set(0,st.peek().get(0)-1); chk=-1;break;}
+            }
+
+            if(chk==1){
+               ArrayList<Integer> a=new ArrayList<>();
+               a.add(val);
+               a.add(arr[i][0]);
+               st.push(a);
+            }
+
         
-            
-        List<Integer> ans= new ArrayList<>();
-        
-        for(int i=0;i<arr.size();i++){ 
-          ans.add(arr.get(i).get(1));
         }
-        
-        return ans;
-        
-        
-        
-        
-        
+
+    List<Integer> ans=new ArrayList<>();
+    Long an[]=new Long[n];
+    Arrays.fill(an,Long.MAX_VALUE);
+
+    while(!st.isEmpty()){
+        an[mp.get(st.peek().get(1))]=(long)st.pop().get(0);
+    }
+
+    for(long i:an){
+        if(Math.abs(i)<=Integer.MAX_VALUE){ans.add(Math.abs((int)i));}
+    }
+
+    return ans;
+
+
+
+    
+
+
+
+
+
     }
 }
