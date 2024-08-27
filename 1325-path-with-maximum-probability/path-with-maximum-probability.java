f0c934d128468911df59d3b8dfaca62141bf3d52
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2024-08-27 09:50:16 +0530

    Time: 85 ms (10.80%) | Memory: 56.3 MB (85.11%) - LeetSync

---
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.java
@@ -0,0 +1,57 @@
+class Solution {
+    static ArrayList<node> arr[];
+    static class node implements Comparable <node>{
+        int st;double dist;
+        node(int st,double dist){
+            this.st=st;
+            this.dist=dist;
+        }
+
+        public int compareTo(node p){
+            return Double.compare(p.dist,this.dist);
+        }
+
+    }
+
+
+
+
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
+        arr=new ArrayList[n];
+        for(int i=0;i<n;i++){
+            arr[i]=new ArrayList<>();
+        }
+
+        for(int i=0;i<edges.length;i++){
+            int s=edges[i][0],e=edges[i][1];double d=succProb[i];
+            arr[s].add(new node(e,d));
+            arr[e].add(new node(s,d));
+        }
+
+        PriorityQueue<node> pq=new PriorityQueue<>();
+
+        pq.add(new node(start_node,1.0));
+        boolean vis[]=new boolean[n];
+
+        while(!pq.isEmpty()){
+           node curr=pq.remove();
+           if(curr.st==end_node){return curr.dist;}
+           if(vis[curr.st]){continue;}
+           vis[curr.st]=true;
+           System.out.print(curr.st+" ");
+
+           for(int i=0;i<arr[curr.st].size();i++){
+              node ed=arr[curr.st].get(i);
+              int dst=ed.st;
+              if(!vis[dst]){
+                pq.add(new node(dst,curr.dist*ed.dist));
+              }
+           }
+
+        }
+
+
+return (double)0;
+
+    }
+}
