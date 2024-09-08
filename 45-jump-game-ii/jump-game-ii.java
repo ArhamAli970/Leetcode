d3efa3f99229b56b4fa410ef5f2e63fd5977d5ed
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2024-09-08 18:16:08 +0530

    Time: 1 ms (98.07%) | Memory: 45.1 MB (51.87%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,16 +1,19 @@
 class Solution {
     public int jump(int[] nums) {
-        int n=nums.length;
-        int dp[]=new int[n];
+        if(nums.length==1){return 0;}
+        int jump=1,max=nums[0],maxCurr=0;
 
-        for(int i=n-2;i>=0;i--){
-            int ele=nums[i],min=10000000;
-            for(int j=i+1;j<=Math.min(n-1,i+ele);j++){
-                min=Math.min(dp[j],min);
+        for(int i=1;i<nums.length;i++){
+            if(i>max){
+                jump++;
+                max=maxCurr;
+                maxCurr=i+nums[i];
+            }else{
+                maxCurr=Math.max(maxCurr,i+nums[i]);
             }
-            dp[i]=min+1;
         }
-        return dp[0];
-        
+
+        return jump;
+
     }
 }
