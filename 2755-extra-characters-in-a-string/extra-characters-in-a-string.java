3414d1fc1949a0b80a581259a7cf8dc0d8748873
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2024-09-23 14:49:18 +0530

    Time: 46 ms (42.18%) | Memory: 45.3 MB (36.39%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
@@ -1,24 +1,35 @@
 class Solution {
-    public int minExtraChar(String s, String[] dix) {
-        HashSet<String>h= new HashSet<>();
-        for(String i:dix){h.add(i);}
-        int n=s.length();
-        int dp[]= new int[n+1];
+    static int dp[];
+    static int n;
 
-        for(int i=0;i<n;i++){
-            int ans=dp[i]+1;
-            for(int j=i;j>=0;j--){
+public static int call(int i,String s,int dp[],HashSet<String> h){
+   if(i>=n){return 0;}
 
-                if(h.contains(s.substring(j,i+1))){ans=Math.min(dp[j],ans);}
+   if(dp[i]!=-1){return dp[i];}
 
-            }
-            dp[i+1]=ans;
-        }
+   int min=Integer.MAX_VALUE;
+   for(String str:h){
+       if(i+str.length()>n){continue;}
+       String schk=s.substring(i,i+str.length());
+       if(schk.equals(str)){
+          int ans= call(i+str.length(),s,dp,h);
+          min=Math.min(ans,min);
+       }
+   }
 
-// for(int i : dp){
-//     System.out.print(i+" ");
-// }
+   min=Math.min(min,call(i+1,s,dp,h)+1);
 
-return dp[n];
+   return dp[i]=min;   
+
+}
+
+    public int minExtraChar(String s, String[] dict){
+        n=s.length();
+        HashSet<String> h=new HashSet<>();
+        for(int i=0;i<dict.length;i++){h.add(dict[i]);} 
+        dp=new int[n];
+        Arrays.fill(dp,-1);
+
+        return call(0,s,dp,h);
     }
 }
