dd86dda72d49b0567b49fa74c6b12caf7bf2004f
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2024-09-28 23:10:10 +0530

    Time: 6 ms (47.65%) | Memory: 117.5 MB (76.51%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -1,23 +1,20 @@
 class Solution {
-    // public static int call(int i,int j,int grid[][],int k){
-      
     public int countSubmatrices(int[][] grid, int k) {
-       int n=grid.length,m=grid[0].length;
-        int cnt=0;
-        int arr[][]= new int[n][m];
+int cnt=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+            
+                int dig=i-1>=0 && j-1>=0?grid[i-1][j-1]:0;
+                int up=i-1>=0?grid[i-1][j]:0;
+                int lft=j-1>=0?grid[i][j-1]:0;
 
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=0;j<m;j++){
-                sum+=grid[i][j];
-                int above=i-1>=0?arr[i-1][j]:0;
-                arr[i][j]=sum+above;
-                if(arr[i][j]<=k){cnt++;}
+                int sum=up+lft-dig+grid[i][j];
+                grid[i][j]=sum;
+                if(sum<=k){cnt++;}
+
             }
         }
-
-        return cnt;
-       
         
+        return cnt;
     }
 }
