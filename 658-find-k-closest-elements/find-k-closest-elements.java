de031b97f5cd8f9bdf3998f1be3fb89ac549985b
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2024-10-14 17:50:59 +0530

    Time: 3 ms (99.82%) | Memory: 45.6 MB (76.54%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,35 +1,38 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        List<Integer> ans=new ArrayList<>();
-        int i1=-1,min=Integer.MAX_VALUE,n=arr.length;
-        for(int i=0;i<n;i++){
-            if(Math.abs(arr[i]-x)<min){
-                min=Math.abs(arr[i]-x);
-                i1=i;
+        
+        int ileft=0,iright=0,i=0,n=arr.length;
+
+        for(i=0;i<n;i++){
+            if(arr[i]>=x){break;}
+        }
+        ileft=i-1;iright=i;
+        int cntleft=0,cntright=0;
+
+
+        while(cntleft+cntright<k){
+            if(iright>=n){cntleft++;ileft--;}
+            else if(ileft<0){cntright++;iright++;}
+            else{
+                int diff1=Math.abs(x-arr[ileft]);
+                int diff2=Math.abs(x-arr[iright]);
+                if(diff1<=diff2){
+                    ileft--;
+                    cntleft++;
+                }else{
+                    iright++;
+                    cntright++;
+                }
             }
+ 
         }
 
-        // System.out.print(i1);
-
-        int i2=i1+1;
-
-        while(k>0){
-            if(i2>=n){i1--;}
-            else if(i1<0){i2++;}
-            else if(Math.abs(arr[i1]-x)<=Math.abs(arr[i2]-x)){i1--;}
-            else{i2++;}
-            k--;
-        }
-
-        for(int i=i1+1;i<i2;i++){
+List<Integer> ans=new ArrayList<>();
+        for(i=ileft+1;i<iright;i++){
             ans.add(arr[i]);
         }
 
         return ans;
-
-        
-
-    
 
     }
 }
