36b8d11ad0de69e10c5846ae590f45d25f3f5610
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2024-11-11 14:57:00 +0530

    Time: 3 ms (65.79%) | Memory: 44.6 MB (50.99%) - LeetSync

--- 2716-prime-subtraction-operation/prime-subtraction-operation.java
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.java
@@ -1,36 +1,34 @@
 class Solution {
-    static boolean  prime[]; 
-
-    public static void sieveOfErathonoses(){ 
-        for(int i=2;i<=Math.sqrt(prime.length);i++){ 
-            if(!prime[i]) continue;
-            for(int j=i*i;j<prime.length;j+=i){ 
-                prime[j]=false;
-            }
-        }
-    }
-
     public boolean primeSubOperation(int[] nums) {
-int max=0;
-        for(int i=0;i<nums.length;i++){ 
-            max=Math.max(nums[i],max);
-        }
-
-        prime=new boolean[max+1];
-        Arrays.fill(prime,true);
-        prime[0]=prime[1]=false;
-        sieveOfErathonoses();
-
-
         
-        for(int i=nums.length-2;i>=0;i--){ 
-            if(nums[i]>=nums[i+1]){ 
-                for(int j=2;j<prime.length;j++){ 
-                    if(prime[j] && nums[i]-j>0 && nums[i]-j<nums[i+1]){nums[i]=nums[i]-j;break;}
+        boolean nprime[]=new boolean[1001];
+        int pr=nprime.length;
+        
+        for(int i=2;i<=Math.sqrt(pr);i++){
+            if(!nprime[i]){
+                for(int j=i*i;j<pr;j+=i){
+                    nprime[j]=true;
                 }
-                if(nums[i]>=nums[i+1]) return false;
             }
         }
-return true;
+        
+        nprime[0]=true;nprime[1]=true;
+        
+        
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]>=nums[i+1]){
+                for(int j=2;j<nums[i];j++){
+                    if(!nprime[j] && nums[i]-j<nums[i+1]){
+                        nums[i]-=j;
+                        break;
+                    }
+                }
+                
+                if(nums[i]>=nums[i+1]){return false;}
+            }
+        }
+        
+        return true;
+        
     }
 }
