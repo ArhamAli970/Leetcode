beaeb7f1c50dc9933aeaa535cc8af85e3b98a6cb
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2025-03-31 10:49:36 +0530

    Time: 35 ms (47.76%) | Memory: 63.5 MB (32.09%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.java
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.java
@@ -1,26 +1,26 @@
 class Solution {
     public long putMarbles(int[] weights, int k) {
-        PriorityQueue<Long> pq1=new PriorityQueue<>();
-        PriorityQueue<Long> pq2=new PriorityQueue<>(Collections.reverseOrder());
-        
+        // PriorityQueue<Long> pq1=new PriorityQueue<>();
+        // PriorityQueue<Long> pq2=new PriorityQueue<>(Collections.reverseOrder());
         int n=weights.length;
+        long ans[]=new long[n-1];
+        
         for(int i=0;i<n-1;i++){
             long sum=weights[i]+weights[i+1];
-            pq1.add(sum);
-            pq2.add(sum);
-            if(pq1.size()>k-1){pq1.remove();}
-            if(pq2.size()>k-1){pq2.remove();}
+            ans[i]=sum;
         }
 
-        long ans1=0,ans2=0;
-        while(!pq1.isEmpty()){
-            long p1=pq1.remove();
-            long p2=pq2.remove();
-            ans1+=p1;
-            ans2+=p2;
+
+        Arrays.sort(ans);
+
+        long fin=0;
+        for(int i=0;i<k-1;i++){
+            fin-=ans[i];
+            fin+=ans[n-2-i];
         }
 
-        return ans1-ans2;
+
+        return fin;
 
 
         
