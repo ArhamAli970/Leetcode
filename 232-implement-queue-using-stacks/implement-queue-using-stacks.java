a65d76f2631dc096b906e88659730388b259cdaf
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2025-04-05 09:58:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (92.81%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -7,22 +7,22 @@ static Stack<Integer> st2;
     }
     
     public void push(int x) {
-        while(!st1.isEmpty()){st2.push(st1.pop());}
-        st1.push(x);
         while(!st2.isEmpty()){st1.push(st2.pop());}
+        st1.push(x);
+        while(!st1.isEmpty()){st2.push(st1.pop());}
 
     }
     
     public int pop() {
-     return st1.pop();   
+     return st2.pop();   
     }
     
     public int peek() {
-    return st1.peek();
+    return st2.peek();
     }
     
     public boolean empty() {
-        return st1.size()==0;
+        return st2.isEmpty();
     }
 }
 
