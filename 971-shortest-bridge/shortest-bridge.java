adb8588a31d4f6b9877f1bbb8861c93b35dbb87a
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2025-04-06 12:21:11 +0530

    Time: 9 ms (54.46%) | Memory: 45.3 MB (61.15%) - LeetSync

--- 971-shortest-bridge/shortest-bridge.java
+++ 971-shortest-bridge/shortest-bridge.java
@@ -1,95 +1,48 @@
 class Solution {
-    static class node{ 
-        int i, j;
-        node(int i,int j){ 
-            this.i=i;
-            this.j=j;
-        }
+static int n;
+static int arr[][]={{-1,0},{0,1},{0,-1},{1,0}};
+public static void dfs(int i,int j,int grid[][]){
+    if(i<0 || j<0 ||i>=n || j>=n || grid[i][j]==2 || grid[i][j]==0){return;}
+    grid[i][j]=2;
+    for(int k=0;k<4;k++){
+        dfs(i+arr[k][0],j+arr[k][1],grid);
     }
-    static Queue<node> q;
-    static    boolean chk[][];
-    public static void dfs(int i,int j,int grid[][],int val){ 
-        if(i<0 || j<0 || i==grid.length || j==grid.length  ||  grid[i][j]!=1){
-            return;
-        }
-        grid[i][j]=val;
-        // chk[i][j]=true;
-        q.add(new node(i,j));
-        dfs(i+1,j,grid,val);
-        dfs(i,j+1,grid,val);
-        dfs(i,j-1,grid,val);
-        dfs(i-1,j,grid,val);
-
-    }
-
+}
     public int shortestBridge(int[][] grid) {
-        int n=grid.length;
-        q=new LinkedList<>();
-        // chk=new boolean[n][n];
-        int done=-1;
-        int val=-1;
-        for(int i=0;i<n;i++){ 
-            for(int j=0;j<n;j++){ 
-
-                if(grid[i][j]==1){ 
-
-                    dfs(i,j,grid,val); done=1; break;
-                   
+        n=grid.length;
+        // arr={{-1,0},{0,1},{0,-1},{1,0}};
+        int cnt=0;
+        Queue<int[]> q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0 || grid[i][j]==2){continue;}
+                if(cnt==0){
+                    dfs(i,j,grid);
+                    cnt++;
+                }else{
+                    q.add(new int[]{i,j});
                 }
-
-
-
             }
-            if(done==1){break;}
-    
         }
 
-      
-
-int cnt=0;
-        while(!q.isEmpty()){ 
-
-            int size=q.size();
-
-            for(int k=0;k<size;k++){ 
-
-                node curr=q.remove();
-                int i=curr.i,j=curr.j;
-                //   System.out.print(i+""+j+" ");
-
-                if(grid[i][j]==1){return cnt-1;}
-               
-
-                // grid[i][j]=-1;
-
-                if(i-1>=0 && grid[i-1][j]!=-1){
-                    grid[i-1][j]=grid[i-1][j]==1?1:-1;
-                    q.add(new node(i-1,j)); }
-                if(j-1>=0 && grid[i][j-1]!=-1){
-                     grid[i][j-1]=grid[i][j-1]==1?1:-1;
-                    q.add(new node(i,j-1)); 
-                    }
-                if(i+1<n &&  grid[i+1][j]!=-1){
-                     grid[i+1][j]=grid[i+1][j]==1?1:-1;
-                    q.add(new node(i+1,j)); 
-                    }
-                if(j+1<n && grid[i][j+1]!=-1){
-                     grid[i][j+1]=grid[i][j+1]==1?1:-1;
-                    q.add(new node(i,j+1)); 
-                    }
-
-
-                
-
+        cnt=0;
+        while(!q.isEmpty()){
+            int sz=q.size();
+            for(int k=0;k<sz;k++){
+                int[] sx=q.remove();
+                for(int i=0;i<4;i++){
+                    int x=arr[i][0],y=arr[i][1];
+                    if(x+sx[0]<0 || y+sx[1]<0 || x+sx[0]>=n || y+sx[1]>=n || grid[x+sx[0]][y+sx[1]]==grid[sx[0]][sx[1]]){continue;}
+                    if(grid[x+sx[0]][y+sx[1]]==2){return cnt;}
+                    grid[x+sx[0]][y+sx[1]]=1;
+                    q.add(new int[]{x+sx[0],y+sx[1]});
+                }
             }
             cnt++;
-
         }
 
+        return cnt;
 
-return -1;
 
-
-        
     }
 }
