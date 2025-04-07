fd13b13f0c8ee38ccbe74f315abe1b1179e5bf9b
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2025-04-07 15:11:42 +0530

    Time: 57 ms (37.11%) | Memory: 46.8 MB (52.63%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,24 +1,4 @@
 class Solution {
-    static int dp[][];
-
-public static int call(int nums[],int i,int n,int sum){ 
-    if(sum==0){return 1;}
-    if(sum<0 || i==n){return 0;}
-
-    if(dp[i][sum]!=-1){return dp[i][sum];}
-
-    int take=call(nums,i+1,n,sum-nums[i]);
-    int notTake=call(nums,i+1,n,sum);
-
-    dp[i][sum]= (take ==1 || notTake==1)?1:0;
-
-    return dp[i][sum];
-
-
-}
-
-
-
     public boolean canPartition(int[] nums) {
         int sum=0;
         for(int i=0;i<nums.length;i++){ 
@@ -26,15 +6,22 @@ public static int call(int nums[],int i,int n,int sum){
         }
         if(sum%2!=0){return false;}
         sum/=2;
+        int n=nums.length;
+        boolean dp[][]=new boolean[n+1][sum+1];
 
-dp= new int[nums.length][sum+1];
+        for(int i=0;i<n;i++){dp[i][0]=true;}
 
-for(int i[]:dp){ 
-    Arrays.fill(i,-1);
-}
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum;j++){
+                int val=nums[i-1];
+                if(j-val>=0){
+                    dp[i][j]=dp[i-1][j-val];
+                }
+                dp[i][j]=dp[i][j] || dp[i-1][j];
+            }
+        }
 
-
-return call(nums,0,nums.length,sum)==1;
+        return dp[n][sum];
 
 
     }
