e02624fa8bf0d6c342c34fb05e1b72f8ebf6fe7c
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2025-04-18 10:46:50 +0530

    Time: 3 ms (56.86%) | Memory: 43.1 MB (29.89%) - LeetSync

---
+++ 38-count-and-say/count-and-say.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public static StringBuilder call(int n){
+        if(n==1){
+        StringBuilder s=new StringBuilder();
+        s.append("1");return s;
+        }
+        StringBuilder sb=call(n-1);
+        StringBuilder ans=new StringBuilder();
+        int cnt=0;
+        char old=sb.charAt(0);
+        for(int i=0;i<sb.length();i++){
+            if(sb.charAt(i)==old){cnt++;}
+            else{
+                ans.append(String.valueOf(cnt));
+                ans.append(old);
+                old=sb.charAt(i);
+                cnt=1;
+            }
+        }
+        ans.append(String.valueOf(cnt));
+        ans.append(old);
+
+        return ans;
+    }
+
+    public String countAndSay(int n) {
+        return call(n).toString();
+    }
+}
