9f88d0653174d189b2211fe30fc5703553e7a8e5
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2025-05-05 20:00:07 +0530

    Time: 4 ms (71.20%) | Memory: 45.8 MB (54.06%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,58 +1,24 @@
 class Solution {
-    public int leastInterval(char[] tasks, int n) {
 
-        int wait[]= new int[26];
+    public int leastInterval(char[] task, int n) {
+        // int time[]=new int[26];
         int cnt[]=new int[26];
+        int tot=0,ans=0,max=0;
 
-        HashSet<Integer> h= new HashSet<>();
-
-        for(int i=0;i<tasks.length;i++){
-            ++cnt[tasks[i]-'A'];
-            h.add(tasks[i]-'A');
+        for(int i=0;i<task.length;i++){
+            ++cnt[task[i]-'A'];
+            if(cnt[task[i]-'A']==1){tot++;}
+            max=Math.max(cnt[task[i]-'A'],max);
         }
 
-        int t=tasks.length,time=0;
-
-        while(t>0){
-            ++time;
-            int maxIdx=-1;
+        // if(tot>n){return task.length;}
+        
             for(int i=0;i<26;i++){
-                int val=maxIdx==-1?0:cnt[maxIdx];
-                if(cnt[i]>val && wait[i]==0){
-                    maxIdx=i;
-                }
+                if(cnt[i]==max){ans++;}
             }
-
-             
-            for(int i=0;i<26;i++){
-                if(wait[i]>0){--wait[i];}
-            }
-            
-
-            wait[maxIdx]=n;
-            --cnt[maxIdx];
-            --t;
-
-            if(t==0){break;}
-
-            int min=Integer.MAX_VALUE;
-            for(int i=0;i<26;i++){
-               if(h.contains(i) && cnt[i]>0){min=Math.min(wait[i],min);}
-            }
-                        // System.out.println(maxIdx+" "+min);
-
-            for(int i=0;i<26;i++){
-               if(h.contains(i)){wait[i]-=min;}
-            }
-
-            // System.out.print(min+" ");
-
-            time+=min;
-
-
-        }
-
-        return time;
+    
+        return Math.max(task.length,((max-1)*(n+1))+ans);
+        
         
     }
 }
