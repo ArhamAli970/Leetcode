6ae9b125448a0b5219ae749d9fbf6e77ac7e9c4a
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2025-06-10 14:53:56 +0530

    Time: 2 ms (99.94%) | Memory: 54.2 MB (25.29%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,26 +1,15 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        if(n==1){return 1;}
-       HashMap<Integer,HashSet<Integer>> mp= new HashMap<>();
-       int arr[]=new int[n+1];
+        if(trust.length==0 && n==1){return n;}
+        int deg[]=new int[n+1];
+        for(int i[]:trust){
+            ++deg[i[1]];
+            --deg[i[0]];
+        }
 
-       for(int i=0;i<trust.length;i++){
-           if(!mp.containsKey(trust[i][1])){
-               mp.put(trust[i][1],new HashSet<>());
-           }
-           mp.get(trust[i][1]).add(trust[i][0]);
-           ++arr[trust[i][0]];
-       }
-
-       for(Map.Entry<Integer,HashSet<Integer>> e: mp.entrySet()){
-           if(e.getValue().size()==n-1 && arr[e.getKey()]==0){
-               return e.getKey();
-           }
-       }
-
-       return -1;
-
-
-
+        for(int i=0;i<=n;i++){
+            if(deg[i]==n-1){return i;}
+        }
+        return -1;
     }
 }
