edeeb8d8c52ccfefcf49d3c36841ac39e77fab47
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2025-06-14 17:04:25 +0530

    Time: 16 ms (95.32%) | Memory: 55.5 MB (6.04%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -4,13 +4,13 @@ class Solution {
 Arrays.sort(people);
 int cnt=0, i=0,j=people.length-1;
 
-while(i<j){
+while(i<=j){
     if(people[i]+people[j]<=limit){i++;}
     j--;
     cnt++;
 }
 
-if(i==j){cnt++;}
+// if(i==j){cnt++;}
 
 return cnt;
         
