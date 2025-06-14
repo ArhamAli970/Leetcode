dbe91a91cddf98b853b7ca5b12a9c7a3efbd670e
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2025-06-14 20:31:56 +0530

    Time: 29 ms (20.09%) | Memory: 45.6 MB (59.58%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,30 +1,24 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-
-        Stack<Integer> st= new Stack<>();
-
-        for(int i=s.length()-1;i>=0;i--){
-             if(s.charAt(i)!='(' && s.charAt(i)!=')'){continue;}
-
-            else if(s.charAt(i)==')'){st.push(i);}
-
-            else{
-                 if( !st.isEmpty() && s.charAt(st.peek())==')'){st.pop();}
-                 else{st.push(i);}
-            }
-
-        }
-
-StringBuilder sb= new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            if(!st.isEmpty() && st.peek()==i){st.pop(); continue;}
-            else{
-                while(!st.isEmpty() && i>=st.peek()){st.pop();}
-                sb.append(s.charAt(i));
+        
+        Stack<Integer> st=new Stack<>();
+        int n=s.length();
+        for(int i=n-1;i>=0;i--){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                if(!st.isEmpty() && s.charAt(st.peek())==')'){st.pop();}
+                else{st.push(i);}
+            }else if(ch==')'){
+                st.push(i);
             }
         }
-
-
+        
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<n;i++){
+            if(!st.isEmpty() && st.peek()==i){st.pop();}
+            else{sb.append(s.charAt(i));}
+        }
+        
         return sb.toString();
         
     }
