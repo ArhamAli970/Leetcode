a7141c7a8f962a07d0e4c69951e595b58b360539
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2025-06-18 07:36:27 +0530

    Time: 93 ms (5.22%) | Memory: 45.3 MB (9.57%) - LeetSync

--- 1691-minimum-number-of-days-to-disconnect-island/minimum-number-of-days-to-disconnect-island.java
+++ 1691-minimum-number-of-days-to-disconnect-island/minimum-number-of-days-to-disconnect-island.java
@@ -1,63 +1,58 @@
 class Solution {
-    static int n,m;
 
-    public static void dfs(int i,int j,int chk[][]){
-        if(i<0 || j<0 || i>=n || j>=m || chk[i][j]==0){return;}
-        chk[i][j]=0;
-        dfs(i-1,j,chk);dfs(i+1,j,chk);dfs(i,j-1,chk);dfs(i,j+1,chk);
-    }
-
-    public static int checkConnect(int grid[][]){
-        int chk[][]=new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                chk[i][j]=grid[i][j];
-            }
+    public static int comp(int i,int j,int grid[][],boolean vis[][]){
+        if(i<0 || j<0 || i>=vis.length || j>=vis[0].length || vis[i][j] || grid[i][j]==0){return 0;}
+        int grd[][]={{0,1},{1,0},{0,-1},{-1,0}};
+        int cnt=1;
+        vis[i][j]=true;
+        for(int k=0;k<4;k++){
+            cnt+=comp(i+grd[k][0],j+grd[k][1],grid,vis);
         }
+        return cnt;
+    }
+    // public static boolean chk(int i,int j,int grid[][]){
+    //     if(j-1<0 || j+1>=grid[0].length){return false;}
+    //     boolean vis[][]=new boolean[n][m];
 
-int cnt=0;
+    // }
+    public int minDays(int[][] grid) {
+        int n=grid.length,m=grid[0].length;
+        
+        int cnt=0;
+        boolean vis[][]=new boolean[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(chk[i][j]==1){
-                    dfs(i,j,chk);
-                    cnt++;
+                if(grid[i][j]==1 && !vis[i][j] && cnt>0){return 0;}
+                else if(grid[i][j]==1){
+                    cnt+=comp(i,j,grid,vis);
                 }
             }
         }
 
-        return cnt;
-
-    }
-
-
-    public static int testMore(int grid[][]){
-        if(checkConnect(grid)==1){
-            return 1;
-        }
-
-        return 0;
-    }
-
-    public int minDays(int[][] grid) {
-        n=grid.length;m=grid[0].length;
-
-        if(checkConnect(grid)!=1){return 0;}
-
-        int min=3;
-
+        if(cnt<2){return cnt;}
+        
+        int grd[][]={{0,1},{1,0},{0,-1},{-1,0}};
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
+            
                 if(grid[i][j]==1){
+                    
+                    int tot=comp(i,j,grid,new boolean[n][m]);
                     grid[i][j]=0;
-                    min=Math.min(min,testMore(grid)+1);
+                    int newi=-1,newj=-1;
+                    for(int k=0;k<4;k++){
+                        if(i+grd[k][0]>=0 && i+grd[k][0]<n && j+grd[k][1]>=0 && j+grd[k][1]<m && grid[i+grd[k][0]][j+grd[k][1]]==1){
+                            newi=i+grd[k][0];
+                            newj=j+grd[k][1];
+                            break;
+                        }
+                    }
+                    // System.out.print(tot);
+                    if(tot-1!=comp(newi,newj,grid,new boolean[n][m])){return 1;}
                     grid[i][j]=1;
                 }
             }
         }
-
-        return min;
-
-
-        
+        return 2;
     }
 }
