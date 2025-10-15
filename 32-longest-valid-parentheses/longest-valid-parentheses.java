c13564bbbf6f3eee4145e25dd3905005f645ce2c
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2025-10-15 23:46:23 +0530

    Time: 11 ms (7.97%) | Memory: 44.8 MB (69.45%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -1,29 +1,32 @@
 class Solution {
     public int longestValidParentheses(String s) {
 
+        int n=s.length(),max=0;
         Stack<Integer> st=new Stack<>();
-        int n=s.length(),sum=0,max=0;
+
         for(int i=0;i<n;i++){
             char ch=s.charAt(i);
-            if(ch==')'){
-                if(!st.isEmpty() && st.peek()==-1){
-                    sum+=2;
-                    st.pop();
-                }else{
-                    sum=0;
-                    st.clear();
-                }
-                if(!st.isEmpty() && st.peek()>0){sum+=st.peek();st.pop();}
+            if(ch=='('){
+                st.push(1);
             }else{
-                if(sum>0){;
-                st.push(sum);
-                sum=0;
+                if(st.isEmpty()){st=new Stack<>();}
+                else{
+                    if(st.peek()==1){st.pop();st.push(2);}
+                    else{
+                        int tp=st.pop();
+                        if(!st.isEmpty() && st.pop()==1){st.push(tp+2);}
+                        else{st=new Stack<>();}
+                    }
+                    while(!st.isEmpty() && st.peek()>1){
+                        int tp=st.pop();
+                        if(!st.isEmpty() && st.peek()>1){st.push(tp+st.pop());}
+                        else{st.push(tp);break;}
+                    }
+                    // System.out.println(st);
+                    if(!st.isEmpty())max=Math.max(st.peek(),max);
                 }
-                st.push(-1);
             }
-            max=Math.max(sum,max);
         }
-
 
         return max;
         
