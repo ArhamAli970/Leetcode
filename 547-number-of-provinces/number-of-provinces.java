09518305657220e4cee349aec83c8eddde5f7eba
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2025-11-19 14:09:25 +0530

    Time: 2 ms (48.28%) | Memory: 47.2 MB (78.49%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,42 +1,50 @@
 class Solution {
-    static int par[];
-    public static int getPar(int v){
-        if(par[v]==v){return v;}
-        return par[v]=getPar(par[v]);
+
+    public static int chkPar(int ele,int par[]){
+        if(par[ele]==ele){return ele;}
+        return par[ele]=chkPar(par[ele],par);
     }
 
     public int findCircleNum(int[][] isConnected) {
-        int n=isConnected.length,cnt=n;
-        par=new int[n];
-        int sz[]=new int[n];
-
+        // here we need to check the groups and the best algo for this is DSU
+        int n=isConnected.length;
+        int par[]=new int[n]; 
+        int rank[]=new int[n];
+        // Arrays.fill(par,-1);
         for(int i=0;i<n;i++){
             par[i]=i;
         }
 
-        Arrays.fill(sz,1);
-
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
+            for(int j=0;j<i;j++){
                 if(isConnected[i][j]==1){
-                    int p1=getPar(i);
-                    int p2=getPar(j);
 
-                    if(p1==p2){continue;}
+                    int p1=chkPar(i,par);
+                    int p2=chkPar(j,par);
 
-                    cnt--;
-                    if(sz[p1]>sz[p2]){
-                        sz[p1]+=sz[p2];
-                        par[p2]=p1;
-                    }else{
-                        sz[p2]+=sz[p1];
-                        par[p1]=p2;
+                    if(p1!=p2){
+                        if(rank[p1]>rank[p2]){
+                            par[p2]=p1;
+                            rank[p1]+=p2;
+                        }else{
+                            par[p1]=p2;
+                            rank[p2]+=p1;
+                        }
                     }
 
                 }
             }
         }
+
+        HashSet h=new HashSet<>();
+        for(int i=0;i<n;i++){
+            par[i]=chkPar(i,par);
+            h.add(par[i]);
+        }
+
+        return h.size();
+
         
-        return cnt;
+
     }
 }
