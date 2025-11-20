33e4cfb24cdefbc119b96444a6ab3f62fef033e8
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2025-11-20 13:50:15 +0530

    Time: 4 ms (99.02%) | Memory: 62.9 MB (14.71%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,33 +1,27 @@
 class Solution {
 
 
-    public static boolean call(int ed,int[][] graph,boolean vis[],int dp[]){
-        if(vis[ed]){return false;}
-        if(dp[ed]!=0){return dp[ed]==-1?false:true;}
+    public static int call(int ed,int[][] graph,boolean vis[],int dp[]){
+        if(dp[ed]!=0){return dp[ed];}
+        if(vis[ed]){return -1;}
         vis[ed]=true;
-        int cNot=0;
+        int ck=1;
         for(int j=0;j<graph[ed].length;j++){
-            if(!call(graph[ed][j],graph,vis,dp)){
-                cNot++;
-            }
-        }
-        dp[ed]=cNot>=1?-1:1;
-        vis[ed]=false;
-        return dp[ed]==-1?false:true;
-        
-
+            ck=call(graph[ed][j],graph,vis,dp);
+            if(ck==-1){break;}
+        }        
+        return dp[ed]=ck;
     }
 
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int n=graph.length;
         ArrayList<Integer> ans=new ArrayList<>();
         int dp[]=new int[n];
+        boolean vis[]=new boolean[n];
 
         for(int i=0;i<n;i++){
-            boolean vis[]=new boolean[n];
-            boolean chk=call(i,graph,vis,dp);
-            if(chk){ans.add(i);}
-    
+            int chk=call(i,graph,vis,dp);
+            if(chk==1){ans.add(i);}
         }
 
         return ans;
